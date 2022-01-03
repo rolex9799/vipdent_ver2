dbb7249b1d988cc06cccf12a0b5e76337d854e13
rolex9799 <68864431+rolex9799@users.noreply.github.com>
2022-01-03 18:48:51 +0200

    3rd commit
    Adding style.css + script.js
    
    Testing if php works :)

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
 
-
-echo "Hello World";
-
-?> 
+  <style>
+    <?php include 'style.css'; ?>
+  </style>
+</head>
+<body>
+  
+<?php echo "Welcome to my page!"; ?>
+</body>
+</html>
