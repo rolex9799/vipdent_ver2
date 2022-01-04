a87d68e73a9a65aec4b69ecf5d8059c30c4257b4
rolex9799 <68864431+rolex9799@users.noreply.github.com>
2022-01-04 12:30:41 +0200

    9th commit
    working on SQL.

---
+++ page/contact.php
@@ -0,0 +1,49 @@
+<div class="banner">
+
+<h1>Sazinities ar mums!</h1>
+
+</div>
+
+<div class="form-panel">
+
+<h1>
+  Aizsūtitiet mums ziņu!
+</h1>
+
+
+<form method="POST" action="create-message.php">
+
+<div class="form-group">
+  <label for="name">Vārds: </label>
+
+  <input type="text" id="name" name="name">
+
+</div>
+
+<div class="form-group">
+  <label for="surname">Uzvārds: </label>
+
+  <input type="text" id="surname" name="surname">
+
+</div>
+
+<div class="form-group">
+
+  <label for="email">E-pasts:</label>
+
+  <input type="text" id="email" name="email">
+</div>
+
+
+<div class="form-group">
+  <label for="message">Ziņa: </label>
+
+  <textarea name="message" id="message" cols="50" rows="10" style="max-width: 370px; min-width: 370px; max-height: 200px; min-height: 200px"></textarea>
+</div>
+
+
+<input class="submit btn" type="submit" value="Nosūtīt">
+
+</form>
+
+</div>
