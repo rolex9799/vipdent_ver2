ad76cc2dd43c1e0156281891f2647f64258c81b0
rolex9799 <68864431+rolex9799@users.noreply.github.com>
2022-01-04 11:49:56 +0200

    7th commit

---
+++ partials/nav-bottom.php
@@ -0,0 +1,28 @@
+<footer>    
+    
+<div class="footer-content">
+
+  <div class="media-items">
+    
+        <a href="https://www.instagram.com/rolex5799/" class="link-socials" target="_blank">
+          <i class="fab fa-instagram"></i>
+          Instagram
+        </a>
+
+        <a href="https://www.facebook.com/rolands.repetto" target="_blank" class="link-socials">
+          <i class="fab fa-facebook"></i>
+          Facebook
+        </a>  
+
+        <a href="https://www.linkedin.com/in/rolands-repetto" target="_blank" class="link-socials">
+          <i class="fab fa-linkedin"></i>
+          Linkedin
+        </a>  
+ 
+  </div>
+  
+</div>
+
+
+</footer>
+ 
