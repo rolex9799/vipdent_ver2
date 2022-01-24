fc9b3979b0cc9fa0cb008445ddabc1a316906740
Clockwork25 <67840273+Clockwork25@users.noreply.github.com>
2022-01-24 14:28:56 +0000

    Animation js

--- partials/index-content.php
+++
@@ -1,7 +0,0 @@
-<div class="ins">
-    <strong>Sadarbībā ar:</strong>
-    <ul>
-        <li><img src="assets/compensa.png"></img></li>
-        <li><img src="assets/gjensidig.png"></img></li>
-    </ul>
-</div>
