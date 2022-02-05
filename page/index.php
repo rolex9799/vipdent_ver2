994b5759c6ea0aa8b162e80947cb69ce2691108a
Clockwork25 <67840273+Clockwork25@users.noreply.github.com>
2022-02-05 04:37:52 +0000

    Added visuals and navbar fix
    Added fade-in picture;
    Fixed navbar responsive positioning in iphones;
    Navbar is fullscreen on small screens;

--- page/index.php
+++ page/index.php
@@ -1,4 +1,5 @@
 <main>
+	<img class="bubble1" src="assets/vp5.jpeg">
 	<div class="body-container">
 		<div class="banner">
 			<h1>Sveicināti klīnikā VipDent!</h1>
@@ -6,7 +7,7 @@
 
 			<div class="info1">Profesionāla attieksme</div>
 			<div class="info2">Augstas kvalitātes zobārstniecība</div>
-			<div class="info3">Ar modernu Itālijā un Vācijā ražotu aprīkojumu!</div>
+			<div class="info3">Ar modernu Itālijā un Vācijā ražotu aprīkojumu</div>
 
 		<div class="contact-us">
 			<h3>Sazinies ar mums!</h3>
